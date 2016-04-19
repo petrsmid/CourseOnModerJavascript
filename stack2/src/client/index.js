6ff3236ce3b2e267030f1120f468aecbc308d996
Petr <petr.smid@wipro.com>
2016-04-19 18:05:33 +0530

    Adding the right stack

---
+++ stack2/src/client/index.js
@@ -0,0 +1,18 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './main';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+const app = document.getElementById('app-root');
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+
+ReactDOM.render(<Main />, app);
+
